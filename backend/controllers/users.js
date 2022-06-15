ef4bff114baf9d22019090f330ebc87cfd628a18
BiggoDot <ovl1work2@gmail.com>
2022-06-15 11:33:01 +0300

    comments removed

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -126,11 +126,7 @@ module.exports.login = (req, res, next) => {
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      // res.cookie('jwt', token, {
-      //   maxAge: 604800000,
-      //   httpOnly: true,
-      // })
-      res.send({token});
+      res.send({ token });
     })
     .catch((err) => {
       if (err.name === 'Error') {
